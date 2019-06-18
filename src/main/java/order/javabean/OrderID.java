0972b3ad1814a9dc086bbcacb1bda9cb284a031a
張力升 <jc91110@gmail.com>
2019-06-18 21:51:55 +0800

    add OrderID

---
+++ src/main/java/order/javabean/OrderID.java
@@ -0,0 +1,18 @@
+package order.javabean;
+
+public class OrderID {
+
+
+    /**
+     * oid : weh3412345yrtrdfssgagtwh
+     */
+
+    private String oid;
+    public String getoid() {
+        return oid;
+    }
+
+    public void setoid(String oid) {
+        this.oid = oid;
+    }
+}
