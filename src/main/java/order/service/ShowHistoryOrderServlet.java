1478648169c8eb13e7f97a10a989e39e2a2f40c4
Tony <tony990309@gmail.com>
2019-06-20 19:48:44 +0800

    排版而已

--- src/main/java/order/service/ShowHistoryOrderServlet.java
+++ src/main/java/order/service/ShowHistoryOrderServlet.java
@@ -14,46 +14,46 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/ShowHistoryOrderServlet")
 public class ShowHistoryOrderServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    
-    public ShowHistoryOrderServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		 
-   	 String year = request.getParameter("year");
-   	String month = request.getParameter("month");
-   	String day = request.getParameter("day");
-   	
-   	 ////////////////////////////////////////// 上面這裡要填老闆端給的參數 決定要拿的菜單狀態 或是 拿到客人ID
-       //String MyStatus ="已完成";
-      
-       MyOrder connection = new MyOrder();
-       
-       //JSONArray jsonArray = connection.getJson();
-       try {
-		connection.getDateOrder(year,month,day);
-	} catch (ParseException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace(); 
+
+	public ShowHistoryOrderServlet() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
-    
 
-       response.setHeader("Access-Control-Allow-Origin", "*");
-       response.setCharacterEncoding("UTF-8");
-       response.setContentType("application/json");
-       response.getWriter().print(connection.getJson());
-       response.getWriter().flush();
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String year = request.getParameter("year");
+		String month = request.getParameter("month");
+		String day = request.getParameter("day");
+
+		////////////////////////////////////////// 上面這裡要填老闆端給的參數 決定要拿的菜單狀態 或是 拿到客人ID
+		//String MyStatus ="已完成";
+
+		MyOrder connection = new MyOrder();
+
+		//JSONArray jsonArray = connection.getJson();
+		try {
+			connection.getDateOrder(year,month,day);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("application/json");
+		response.getWriter().print(connection.getJson());
+		response.getWriter().flush();
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+
 		doGet(request, response);
 	}
 
