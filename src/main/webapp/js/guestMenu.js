4295dd8ada655d3d33032109da3cb7fe2813694a
00557105 <hardychang0131@gmail.com>
2019-06-21 13:20:12 +0800

    json

--- src/main/webapp/js/guestMenu.js
+++ src/main/webapp/js/guestMenu.js
@@ -144,7 +144,7 @@ $(document).ready(function() {
             $.ajax({
                 url: "https://fisystem.herokuapp.com/AddOrderServlet",
                 data: jsonData, //data只能指定單一物件
-                dataType: "text",
+                dataType: "json",
                 type: "Post",
                 success: function() {
                     alert("新增成功！");
